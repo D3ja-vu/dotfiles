6e63b42a0cd7af9ab5bc41084060f22079f9cd41
Abdullah Muhamed <164979079+D3ja-vu@users.noreply.github.com>
2024-10-18 21:48:59 +0300

    Add files via upload

--- ignis/modules/bar/battery.py
+++ ignis/modules/bar/battery.py
@@ -1,35 +1,51 @@
+import psutil
 from ignis.widgets import Widget
-from ignis.services.upower import UPowerService, UPowerDevice
 
-upower = UPowerService.get_default()
+# Unicode bolt symbol
+BOLT_SYMBOL = "⚡"
 
+def get_battery_info():
+    battery = psutil.sensors_battery()
+    if battery:
+        return battery.percent, battery.power_plugged
+    return None, None
 
-def battery_item(device: UPowerDevice) -> Widget.Box:
-    return Widget.Box(
-        css_classes=["battery-item"],
-        setup=lambda self: device.connect("removed", lambda x: self.unparent()),
-        child=[
-            Widget.Icon(
-                icon_name=device.bind("icon_name"), css_classes=["battery-icon"]
-            ),
-            Widget.Label(
-                label=device.bind("percent", lambda x: f"{int(x)}%"),
-                css_classes=["battery-percent"],
-            ),
-            Widget.Scale(
-                min=0,
-                max=100,
-                value=device.bind("percent"),
-                sensitive=False,
-                css_classes=["battery-scale"],
-            ),
-        ],
+def battery_item() -> Widget.Box:
+    battery_percent, is_plugged = get_battery_info()
+
+    if battery_percent is None:
+        battery_percent = 0  # If no battery info, set to 0%
+        is_plugged = False
+
+    battery_label = Widget.Label(
+        label=BOLT_SYMBOL if is_plugged else f"{int(battery_percent)}%",
+        css_classes=["battery-percent"],
     )
 
+    battery_scale = Widget.Scale(
+        min=0,
+        max=100,
+        value=battery_percent,
+        sensitive=False,
+        css_classes=["battery-scale"],
+    )
+
+    box = Widget.Box(
+        css_classes=["battery-item"],
+        child=[battery_label, battery_scale]
+    )
+
+    def refresh():
+        new_battery_percent, new_is_plugged = get_battery_info()
+        battery_label.label = BOLT_SYMBOL if new_is_plugged else f"{int(new_battery_percent)}%"
+        battery_scale.value = new_battery_percent
+        return True
+
+    box.setup = lambda self: GLib.timeout_add_seconds(3, lambda: refresh())  # Refresh every 3 seconds
+
+    return box
 
 def battery_widget() -> Widget.Box:
     return Widget.Box(
-        setup=lambda self: upower.connect(
-            "battery-added", lambda x, device: self.append(battery_item(device))
-        ),
+        setup=lambda self: self.append(battery_item())
     )
